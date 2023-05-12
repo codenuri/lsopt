e7c42560c1a60b61f73d5426dad181d67b698f3f
ecourse <smkang@codenuri.co.kr>
2023-05-12 17:07:10 +0900

    Add files via upload

--- DAY2/라이브러리3.c
+++ DAY2/라이브러리3.c
@@ -8,16 +8,19 @@ int foo(int a, int b)
 	printf("foo");
 	return 0;
 }
-
+int __stdcall goo(int a, const char* b , const char* c, int n)
+{
+	printf("goo");
+	return 0;
+}
 int main()
 {
 	// .idata section 의 메모리를 쓰기 가능하게 변경
 	DWORD old;
-	VirtualProtect((void*)0x주소, PAGE_READWRITE, &old);
+	VirtualProtect((void*)0x041B098, 100,  PAGE_READWRITE, &old);
 
+	*((int*)0x041B098) = (int)&goo;
 
-	*((int*)0x주소) = (int)&foo;
-
-
-	mul(1, 2);
+//	mul(1, 2);
+	MessageBoxA(0, "aa", "bb", 0);
 }
